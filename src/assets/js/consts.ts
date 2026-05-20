23d741e49fb7190c00be325e173f86e1d1197517
Stanislav Yem <002yem@gmail.com>
2026-05-20 14:59:34 -0500

    Add assets

---
+++ src/assets/js/consts.ts
@@ -0,0 +1,71 @@
+import { TInputValueTypes } from "@/interfaces"
+
+export const scrollThreshold = 1; //in screen heights
+
+export const screenNames = ['start','xs', 'sm', 'md', 'lg', 'xl', 'xxl'] as const
+
+export const screenSizes: Record<typeof screenNames[number], number> = {
+	xxl: 1536,
+	xl: 1280,
+	lg: 1024,
+	md: 768,
+	sm: 640,
+	xs: 480,
+	start: 1
+}
+
+export const linkHome = '/home'
+export const linkPrivacy = '/privacy-policy'
+export const linkTerms = '/terms-conditions'
+export const linkAbout = '/about-us'
+export const linkFeedback = '/feedback'
+interface IInputFilter {
+	length: {
+		min: number,
+		max: number
+	},
+	type: TInputValueTypes
+} 
+
+export const inputPolicies: Record<string, IInputFilter> = {
+	name: {
+		length: {
+			min: 2,
+			max: 60
+		},
+		type: 'name'
+	},
+	email: {
+		length: {
+			min: 6,
+			max: 100
+		},
+		type: 'email'
+	},
+	message: {
+		length: {
+			min: 20,
+			max: 5000
+		},
+		type: 'skip'
+	},
+	password: {
+		length: {
+			min: 8,
+			max: 30
+		},
+		type: 'password'
+	}
+}
+
+export const requests = {
+	sendEmail: {
+		url: `example/api/email`,
+		method: 'POST'
+	}
+}
+
+// z-index:
+
+// header: 2000
+// #skiper: 5000
